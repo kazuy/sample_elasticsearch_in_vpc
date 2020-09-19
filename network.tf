371b0b7d560374551c5e4678e6da8b97aff5859e
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-09-19 16:00:58 +0900

    ルートテーブルを作成

--- network.tf
+++ network.tf
@@ -22,3 +22,19 @@ resource "aws_internet_gateway" "main" {
     Name = "Elasticsearch and RDP sample Internet Gateway"
   }
 }
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "Elasticsearch and RDP sample Route table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
