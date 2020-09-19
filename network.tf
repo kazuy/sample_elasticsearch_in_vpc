bc852ee6304ea6db9cfff0423b4eccfe7f5099aa
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-09-19 15:47:42 +0900

    サブネットを作成

--- network.tf
+++ network.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
     Name = "Elasticsearch and RDP sample VPC"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "12.0.1.0/24"
+
+  tags = {
+    Name = "Elasticsearch and RDP sample Subnet"
+  }
+}
