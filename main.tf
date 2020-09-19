a49d408bdede6bbeaee8a472d20a9e442dd3ff17
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-09-19 16:14:12 +0900

    MyIPAddressをRDPに追加

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ provider "aws" {
   region  = "ap-northeast-1"
   version = "= 3.7.0"
 }
+
+provider http {
+  version = "= 1.2.0"
+}
--- security_group.tf
+++ security_group.tf
@@ -24,3 +24,22 @@ resource "aws_security_group_rule" "rdp_to_es" {
   security_group_id        = aws_security_group.elasticsearch.id
   source_security_group_id = aws_security_group.rdp.id
 }
+
+# see: https://dev.classmethod.jp/articles/reference-my-pubic-ip-in-terraform/
+data http ifconfig {
+  url = "https://ifconfig.co/ip"
+}
+
+locals {
+  current-ip   = chomp(data.http.ifconfig.body)
+  allowed-cidr = "${local.current-ip}/32"
+}
+
+resource "aws_security_group_rule" "connect_to_rdp" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.rdp.id
+  cidr_blocks       = [local.allowed-cidr]
+}
