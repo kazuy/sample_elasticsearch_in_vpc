15a0b98131f2d18bf1fb7cad5cb31fe0f1281db8
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-09-19 16:38:51 +0900

    Elasticsearchを追加

---
+++ elastic_search.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_service_linked_role" "main" {
+  aws_service_name = "es.amazonaws.com"
+}
+
+resource "aws_elasticsearch_domain" "main" {
+  domain_name           = "sample-elasticsearch"
+  elasticsearch_version = "7.7"
+
+  cluster_config {
+    instance_type = "t2.small.elasticsearch"
+  }
+
+  vpc_options {
+    subnet_ids         = [aws_subnet.main.id]
+    security_group_ids = [aws_security_group.elasticsearch.id]
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp2"
+    volume_size = 10
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = 18
+  }
+
+  tags = {
+    Domain = "Sample Elasticsearch"
+  }
+
+  depends_on = [aws_iam_service_linked_role.main]
+}
+
+resource "aws_elasticsearch_domain_policy" "main" {
+  domain_name = aws_elasticsearch_domain.main.domain_name
+
+  access_policies = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action    = "es:*"
+          Effect    = "Allow"
+          Principal = "*"
+          Resource  = "${aws_elasticsearch_domain.main.arn}/*"
+        },
+      ]
+    }
+  )
+}
